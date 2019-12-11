0db566c49f194b12b0d3741d99fe867a9fc279b8
Gift Mugweni <giftmugweni@gmail.com>
2019-12-11 16:27:14 +0200

    bad practice

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    gooogle:{
+        clientID: "990626129039-uo64cn82p606p80i8cotegv7satljfms.apps.googleusercontent.com",
+        clientSecret: "o118rnuEBjW4uOx81AQJWquA"
+    },
+    mongodb:{
+        dbURI: 'mongodb+srv://test:test@todo-9zzzh.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    session:{
+        cookieKey: 'the net ninja is awesome I gues, maybe you know'
+    }
+}
